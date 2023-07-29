d7d6f75366d041c958846c755fd2699ddda5acf4
devonium <59941578+devonium@users.noreply.github.com>
2023-07-29 19:33:51 +0300

    Added [CMD] [CFG] bindmenu_updatepage bindmenu_reopen

--- README.md
+++ README.md
@@ -3,13 +3,14 @@ A simple tool that generates a cs 1.6 styled command menu config for source engi
 https://nav.dl.sourceforge.net/project/luabinaries/5.4.2/Tools%20Executables/lua-5.4.2_Win32_bin.zip
 
 
-![](https://i.imgur.com/fBZlp3y.png)
+![](https://i.imgur.com/ymN2UcB.png)
+![](https://i.imgur.com/uPPNieN.png)
 ```
 lua54.exe commandmenu_generator.lua example.txt
 ```
 
-![](https://i.imgur.com/1n2mqmd.png)
-![](https://i.imgur.com/UYXr8eX.png)
+![](https://i.imgur.com/UhDYwLN.png)
+![](https://i.imgur.com/MvAcvyW.png)
 ```
 lua54.exe commandmenu_generator.lua example.txt -extended
 ```
--- commandmenu_generator.lua
+++ commandmenu_generator.lua
@@ -29,11 +29,14 @@ alias bindmenu_restore_19 "bind 1 slot1;bind 2 slot2;bind 3 slot3;bind 4 slot4;b
 
 alias bindmenu_bindopenkey "" 
 alias bindmenu_bindclosekey "" 
+alias bindmenu_currentpage bindmenu_page1
 ]] ..
 (ExtendedMenu and ([[alias bindmenu_printpage "clear;con_filter_enable 0;wait 5;bindmenu_slot1;bindmenu_slot2;bindmenu_slot3;bindmenu_slot4;bindmenu_slot5;bindmenu_slot6;bindmenu_slot7;bindmenu_slot8;wait 5;con_filter_enable 1;"]]) or ([[alias bindmenu_printpage "clear;con_filter_enable 0;wait 5;bindmenu_slot1;bindmenu_slot2;bindmenu_slot3;bindmenu_slot4;bindmenu_slot5;bindmenu_slot6;wait 5;con_filter_enable 1;""]])) ..
 [[
 
 alias bindmenu_open "bindmenu_bindclosekey;wait 25; bindmenu_override_46;developer 1;con_notifytime 10000;bindmenu_page1"
+alias bindmenu_reopen "wait 1;bindmenu_bindclosekey;wait 25; bindmenu_override_46;developer 1;con_notifytime 10000;bindmenu_currentpage"
+alias bindmenu_updatepage "bindmenu_currentpage"
 alias bindmenu_close "bindmenu_bindopenkey;developer 0;con_filter_enable 0;con_notifytime 8;bindmenu_restore_19"
 alias bindmenu_next ""
 alias bindmenu_prev ""
@@ -72,6 +75,9 @@ local lines = {}
 
 local header = ""
 
+local cfgMode = false
+local postCfg = ""
+
 for line in io.lines(filename) 
 do
 	header = header .. "//" .. line .. "\n"
@@ -80,13 +86,32 @@ do
 		local key = line:match("%[KEY:[%s]*([^%]%s]*)")
 		l = l .. 'alias bindmenu_bindopenkey "bind '.. key ..' bindmenu_open"\n'
 		.. 'alias bindmenu_bindclosekey "bind '.. key ..' bindmenu_close"\n'
+	elseif line:match("%[CFG%]")
+	then
+		cfgMode = true
+	elseif line:match("%[CMD%]")
+	then
+		cfgMode = false
 	else
-		local name, cmd = line:match("%[(.*)]"), line:match("%[.*][%s]?(.*)")
-		lines[#lines + 1] = {name,cmd}
+		if cfgMode
+		then
+			line = line:gsub("{CURRCMD_PAGE}", math.ceil(#lines / menuLinesCount))
+			line = line:gsub("{CURRCMD_SLOT}", #lines % (menuLinesCount+1))
+			line = line:gsub("{CURRCMD_NAME}", #lines % (menuLinesCount+1) .. '.' .. (lines[#lines])[1]:gsub(" ", "."))
+			postCfg = postCfg .. line .. "\n"
+		else
+			local name, cmd = line:match("%[(.*)]"), line:match("%[.*][%s]?(.*)")
+			if name and cmd
+			then
+				lines[#lines + 1] = {name,cmd}
+			end
+		end
 	end
 end
 
-l = header .. "\n//////\n" .. l
+
+
+l = "////// SOURCE BEG //////\n" .. header .. "////// SOURCE END //////\n\n" .. l
 
 local line_ind = 1
 local lines_count = #lines
@@ -96,8 +121,8 @@ local page_count = math.ceil(lines_count / menuLinesCount)
 
 while line_ind <= lines_count
 do
-	l = l .. "\n\n//////PAGE " .. page_ind.. "\n"
-	local bindmenu_page = 'alias bindmenu_page' .. page_ind .. ' "'
+	l = l .. "\n\n////// PAGE " .. page_ind.. " //////\n"
+	local bindmenu_page = 'alias bindmenu_page' .. page_ind .. ' "alias bindmenu_currentpage' .. ' bindmenu_page'.. page_ind .. ';'
 	for i = 1, menuLinesCount
 	do
 		local name = (line_ind <= lines_count) and (lines[line_ind][1]) or ("EMPTY")
@@ -114,14 +139,14 @@ do
 	l = l .. 'alias bindmenu_page' .. page_ind .. '_prev ' .. ((page_ind == 1) and ('""') or ('bindmenu_page' .. (page_ind - 1))) .. '\n'
 	l = l .. 'alias bindmenu_page' .. page_ind .. '_next ' .. ((line_ind <= lines_count) and ('bindmenu_page' .. (page_ind + 1)) or ('""')) .. '\n'
 	
-	l = l .. bindmenu_page .. 'alias bindmenu_next' ..	' bindmenu_page'.. page_ind .. '_next ' .. ';alias bindmenu_prev' ..' bindmenu_page'.. page_ind .. '_prev ' .. ';alias bindmenu_slot'..(ExtendedMenu and 8 or 6)..' echo "'..(ExtendedMenu and 8 or 6)..'.EXIT.[' .. page_ind .. '/' .. page_count .. ']"' .. ';bindmenu_printpage"\n' 
-
+	l = l .. bindmenu_page .. "bindmenu_page".. page_ind .. '_setup;' .. ';alias bindmenu_slot'..(ExtendedMenu and 8 or 6)..' echo "'..(ExtendedMenu and 8 or 6)..'.EXIT.[' .. page_ind .. '/' .. page_count .. ']"' .. ';bindmenu_printpage"\n' 
+	l = l .. "alias bindmenu_page".. page_ind .. '_setup "' .. 'alias bindmenu_next' ..	' bindmenu_page'.. page_ind .. '_next ' .. ';alias bindmenu_prev' ..' bindmenu_page'.. page_ind .. '_prev ' .. '"'
 	page_ind = page_ind + 1
 end
 
 
 l = l .. "\nbindmenu_bindopenkey\n"
-
+l = l .. "\n////// CFG //////\n" .. postCfg
 local file = io.open("cmdmenu" .. ".cfg", "wb")
 file:write(l)
 file:close()
